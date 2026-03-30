e103eb3cba9d61439c35d7c0c6a537bc00eadda8
ilmari <ilmari.luukkainen@gmail.com>
2026-03-30 13:28:31 +0300

    ig ja linkkari korjattu

--- junction/src/Footer.jsx
+++ junction/src/Footer.jsx
@@ -3,14 +3,14 @@ function Footer() {
     return(
         <div className="footer">
             <div className="footer-socials">
-                <a className="social-link icon-swap" href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                <a className="social-link icon-swap" href="https://www.instagram.com/junctionxturku?igsh=N2dhbjJvd2wyMTlw" target="_blank" rel="noopener noreferrer">
                     <span className="icon-wrap">
                         <img className="icon icon-bw" src="/instagram-logo-grey.png" alt="Instagram" />
                         <img className="icon icon-color" src="/instagram-logo.png" alt="" aria-hidden="true" />
                     </span>
                     <span>Instagram</span>
                 </a>
-                <a className="social-link icon-swap" href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                <a className="social-link icon-swap" href="https://www.linkedin.com/company/junction-x-turku/" target="_blank" rel="noopener noreferrer">
                     <span className="icon-wrap">
                         <img className="icon icon-bw" src="/linkedin-logo-grey.png" alt="LinkedIn" />
                         <img className="icon icon-color" src="/linkedin-logo.png" alt="" aria-hidden="true" />
