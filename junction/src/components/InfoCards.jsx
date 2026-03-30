e8357a0f0f08e975a9e4eb684a7a643b3d080dfb
Oskari Kyllönen <omkyll@utu.fi>
2026-03-30 13:46:59 +0300

    moved info cards and partners css to own files

--- junction/src/components/InfoCards.jsx
+++ junction/src/components/InfoCards.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./InfoCards.css"
 
 const InfoCards = () => {
   return (
--- junction/src/components/Partners.jsx
+++ junction/src/components/Partners.jsx
@@ -1,5 +1,6 @@
 
 import Card from "./Card";
+import "./Partners.css"
 
 const PARTNERS = [
   { 
