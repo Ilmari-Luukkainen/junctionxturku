ff9c9c8e856f9ef24140000cf6eb3d3816cd5d3d
ilmari <ilmari.luukkainen@gmail.com>
2026-03-31 16:03:53 +0300

    3

--- junction/src/LineWaves.jsx
+++ junction/src/LineWaves.jsx
@@ -1,7 +1,7 @@
 import { Renderer, Program, Mesh, Triangle } from 'ogl';
 import { useEffect, useRef } from 'react';
 
-import './backround.css';
+import './Backround.css';
 
 function hexToVec3(hex) {
   const h = hex.replace('#', '');
