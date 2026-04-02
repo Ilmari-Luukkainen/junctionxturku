3156d22e813b4e4975acfdcaedbf56a9d4b111e0
ilmari <ilmari.luukkainen@gmail.com>
2026-04-02 14:36:55 +0300

    r

--- junction/vite.config.js
+++ junction/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Lisää tämä rivi. Käytä projektisi nimeä GitLabissa vinoviivojen välissä.
+  base: '/JunctionXTurku/', 
 })
