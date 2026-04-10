fc451da56675d2d6630b67897b2b9cb771b97db2
ilmari <ilmari.luukkainen@gmail.com>
2026-04-10 13:01:14 +0300

    video

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,27 +1,21 @@
 import './Video.css';
+// Tuodaan video tiedostona (varmista, että tiedostonimi on täsmälleen tämä)
+import brandVideo from '/JunctionXbrandmovie.mp4';
 
 export default function Video() {
-    // Tässä on videon ID suoraan antamastasi linkistä
-    const videoId = "-HZa8mh3HgQ"; 
-    
-    // Parametrit: 
-    // autoplay=1 (käynnistyy heti)
-    // mute=1 (pakollinen, jotta autoplay toimii selaimissa)
-    // loop=1 & playlist=ID (nämä kaksi yhdessä tarvitaan, jotta video looppaa)
-    // controls=0 (tekee videosta puhtaamman näköisen taustalle)
-    const youtubeSrc = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&rel=0`;
-
     return (
         <div className="video-wrapper">
-            <iframe
+            <video 
                 className="custom-video"
-                src={youtubeSrc}
-                title="JunctionX brand movie"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-                loading='lazy'
-            ></iframe>
+                autoPlay 
+                muted 
+                loop 
+                playsInline
+                preload="auto"
+            >
+                <source src={brandVideo} type="video/mp4" />
+                Selaimesi ei tue videon toistoa.
+            </video>
         </div>
     );
 }
