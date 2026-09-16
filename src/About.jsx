3e6b3dc23353b4a5ad9fe8d36bfad1269dcabd54
ilmari <ilmari.luukkainen@gmail.com>
2026-09-16 10:22:26 +0300

    säpö

--- src/About.jsx
+++ src/About.jsx
@@ -11,8 +11,8 @@ const About = () => {
           {/* Vaihdettu h2:ksi, koska Landingpage on jo h1. 
               Lisätty JunctionX Turku kontekstia. */}
           <h1>MEET THE TEAM</h1>
-          <a href="mailto:info@junctionxturku.com" className="info-link">
-            <p className="info-subtitle">info@junctionxturku.com</p>
+          <a href="mailto:rafael.raoult@hackjunction.com" className="info-link">
+            <p className="info-subtitle">rafael.raoult@hackjunction.com</p>
           </a> 
         </header>
 
